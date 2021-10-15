4cedd64b16863b06f488c17f09a518613810631b
SabahNur86 <sabahnurdagtekin@gmail.com>
2021-10-15 21:10:36 +0300

    v1

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import About from './components/About'
+import Banner from './components/Banner'
+import Blog from './components/Blog'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Portfolio from './components/Portfolio'
+import Preloader from './components/Preloader'
+import Services from './components/Services'
+import Testimonal from './components/Testimonal'
+
+const App = () => {
+  return (
+    <div>
+      <Preloader/>
+      <Header/>
+      <Banner/>
+      <About/>
+      <Services/>
+      <Portfolio/>
+      <Testimonal/>
+      <Blog/>
+     <Contact/>
+     <Footer/>
+    </div>
+  )
+}
+
+export default App
